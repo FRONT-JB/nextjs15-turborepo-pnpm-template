cf416e1c909fa261facbd974a869054f70874548
전병길 <jeonbg@kakao.com>
2025-02-19 02:41:24 +0900

    chore: Nextjs turborepo pnpm template

---
+++ README.md
@@ -0,0 +1,13 @@
+# Nextjs15 Turborepo
+
+- [x] Turborepo
+- [x] pnpm
+- [x] tailwindcss
+- [x] eslint
+- [x] prettier
+- [x] commitlint
+- [x] release-please
+- [x] commitizen
+- [x] husky
+- [x] Nextjs15
+- [x] shadcn
---
+++ apps/web/app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@ui/button';
+
+export default function Page() {
+  return (
+    <main>
+      <Button>Click me</Button>
+
+      <Button variant="secondary">Click me 2</Button>
+
+      <Button variant="destructive">Click me 3</Button>
+
+      <Button variant="outline">Click me 4</Button>
+
+      <Button variant="ghost">Click me 5</Button>
+    </main>
+  );
+}
